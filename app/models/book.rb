2dd79e1538743b73d4b45f7e8827c970f79abe1c
Taaku0405 <rakosutaa@gmail.com>
2022-05-09 17:36:44 +0000

    Finish-EX-9

--- app/models/book.rb
+++ app/models/book.rb
@@ -10,7 +10,6 @@ class Book < ApplicationRecord
     favorites.where(user_id: user.id).exists?
   end
 
-
    def self.search_for(content, method)
     if method == 'perfect'
       Book.where(title: content)
